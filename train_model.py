c86fae2acbf0e2623f194b44a88f7085b71d7482
saijaswanth664 <saijaswant2518@gmail.com>
2026-03-15 23:52:05 +0530

    Created movie-user matrix for recommendation system

--- train_model.py
+++ train_model.py
@@ -1,18 +1,19 @@
 import pandas as pd
 
-# Load the datasets
+# Load datasets
 movies = pd.read_csv("dataset/movies.csv")
 ratings = pd.read_csv("dataset/ratings.csv")
 
-# Display first few rows
-print("Movies Dataset:")
-print(movies.head())
-
-print("\nRatings Dataset:")
-print(ratings.head())
-
-# Merge the datasets
+# Merge datasets
 data = pd.merge(ratings, movies, on="movieId")
 
-print("\nMerged Dataset:")
-print(data.head())
+# Create movie-user matrix
+movie_matrix = data.pivot_table(index="title", columns="userId", values="rating")
+
+# Replace NaN with 0
+movie_matrix = movie_matrix.fillna(0)
+
+print("Movie User Matrix:")
+print(movie_matrix.head())
+
+print("\nShape of matrix:", movie_matrix.shape)
